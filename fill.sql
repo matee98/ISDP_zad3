f40352d72d4c9578a4f38a2aaa310578417e8d4f
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-19 14:56:53 +0100

    jenkins update #2

--- fill.sql
+++ fill.sql
@@ -1,5 +1,5 @@
 connect 'jdbc:derby:WM;user=WM;password=WM;';
 CREATE SCHEMA WM;
-run 'createDB.sql'
+run 'src/main/resources/createDB.sql'
 CREATE VIEW AUTHENTICATION_VIEW AS;
-run 'initDB.sql'
+run 'src/main/resources/initDB.sql'
--- init.sql
+++ init.sql
@@ -1,5 +1,4 @@
-connect 'jdbc:derby:WM;user=WM;password=WM;';
-CREATE SCHEMA WM;
-run 'src/main/resources/createDB.sql'
-CREATE VIEW AUTHENTICATION_VIEW AS;
-run 'src/main/resources/initDB.sql'
+connect 'jdbc:derby:WM;create=true;' user 'WM';
+CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');
+disconnect;
+exit;
