8aed0ae260dac843dd0e65a84841fda80b951938
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-20 01:25:56 +0100

    updae

--- init.sql
+++ init.sql
@@ -1,7 +1,6 @@
 connect 'jdbc:derby:WM;create=true;' user 'WM';
 CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');
 disconnect;
-exit;
 connect 'jdbc:derby:WM;user=WM;password=WM;';
 CREATE SCHEMA WM;
 run 'src/main/resources/createDB.sql'
