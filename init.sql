cbb92630610482885e194c8409134b6e5220ab46
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-20 01:58:29 +0100

    x

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-connect 'jdbc:derby:WM;create=true;' user 'WM';
+connect 'jdbc:derby://localhost:1527/WM;create=true;' user 'WM';
 CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');
 run 'createDB.sql';
 run 'initDB.sql';
