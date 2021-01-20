1af905bc7e3028d547bb177327d1bb7145b23c98
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-20 01:45:05 +0100

    fix

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');
 disconnect;
 connect 'jdbc:derby:WM;user=WM;password=WM;';
 CREATE SCHEMA WM;
-run 'createDB.sql'
-run 'initDB.sql'
+run 'createDB.sql';
+run 'initDB.sql';
 disconnect;
 exit;
