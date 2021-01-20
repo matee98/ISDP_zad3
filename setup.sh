aed8b018b33d8f8c264857eda205bfa07f2531e7
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-20 02:34:49 +0100

    x

--- setup.sh
+++ setup.sh
@@ -3,17 +3,25 @@
 spawn ./ij
 expect "ij>"
 send "connect 'jdbc:derby:WM;create=true;' user 'WM';\r"
-expect "ij>"
-send "CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');\r"
-expect "ij>"
-send "CREATE SCHEMA WM;\r"
-expect "ij>"
-send "run 'createDB.sql';\r"
-expect "ij>"
-send "run 'initDB.sql';\r"
-expect "ij>"
-send "disconnect;"
-expect "ij>"
-send "exit;"
+expect {
+	"BŁĄD" {
+		expect "ij>"
+        send "exit;\r"
+    }
+    "ij>" {
+        send "CALL SYSCS_UTIL.SYSCS_CREATE_USER('WM', 'WM');\r"
+        expect "ij>"
+        send "CREATE SCHEMA WM;\r"
+        expect "ij>"
+        send "run 'createDB.sql';\r"
+        expect "ij>"
+        send "run 'initDB.sql';\r"
+        expect "ij>"
+        send "disconnect;\r"
+        expect "ij>"
+        send "exit;\r"
+    }
+    
+}
 
 interact
